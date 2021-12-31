c094cdadd287d7c19f57289052718775cd004096
ksawis <ksawis@gmail.com>
2021-12-31 11:04:11 +0100

    Dodałam main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include <unistd.h>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+
+  int liczba, tab[6];
+  srand(time(NULL));
+
+  for (int i = 1; i<=6; i++)
+  {
+  tab[i] = rand()%49+1;
+  sleep(1);
+  cout << tab[i] << endl;
+  }
+
+
+    return 0;
+}
